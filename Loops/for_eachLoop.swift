cd2d2cbe6ec82a74c1f4259e624520ac85ade9e0
AdiByte30 <adityaandhale30@gmail.com>
2025-08-19 15:31:35 +0530

    for each loops

---
+++ Loops/for_eachLoop.swift
@@ -0,0 +1,14 @@
+
+let numbers = [2,7,4,9,5,1,3,6,8]
+
+//for x in numbers {
+    //print(x,x,x,x,separator: " : ")
+//}
+
+// numbers.foreach { number in
+
+//     print($0, $0, $0, $0, separator: " : ")
+// }
+numbers.forEach { number in
+    print(number)
+}
